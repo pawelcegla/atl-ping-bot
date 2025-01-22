7496364b66571066ce92673fdbfbeeae7b4edbb0
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2025-01-22 12:16:34 +0100

    Create Confluence pages & use API tokens

--- main.py
+++ main.py
@@ -5,32 +5,24 @@ import requests
 import sqlite3
 import sys
 
+def ping_summary():
+    return 'One ping only @ {}'.format(datetime.now(tz=timezone.utc).strftime('%Y-%m-%d %H:%M'))
+
 con = sqlite3.connect(sys.argv[1])
 cur = con.cursor()
-(cloud_id, account_id) = cur.execute('select cloud_id, account_id from config').fetchone()
-(client_id, client_secret) = cur.execute('select client_id, client_secret from app').fetchone()
-jira_refresh_token = cur.execute("select refresh_token from token where product = 'jira'").fetchone()[0]
-print('refreshing access token')
-res = requests.post('https://auth.atlassian.com/oauth/token', json={
-    'grant_type': 'refresh_token',
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'refresh_token': jira_refresh_token
-})
-if res.status_code == 200:
-    print('200 -> updating tokens')
-    cur.execute("update token set access_token = ?, refresh_token = ? where product = 'jira'", (res.json()['access_token'], res.json()['refresh_token']))
-    con.commit()
-    create_issue_res = requests.post('https://api.atlassian.com/ex/jira/{}/rest/api/3/issue'.format(cloud_id),
-                         headers={'Authorization': 'Bearer {}'.format(res.json()['access_token'])}, json={
-            'fields': {'summary': 'One ping only @ {}'.format(datetime.now(tz=timezone.utc).strftime('%Y-%m-%d %H:%M')),
-                       'project': {'key': 'PING'}, 'issuetype': {'id': '10024'}, 'assignee': {'id': account_id}}}).json()
-    print(json.dumps(create_issue_res))
-    print(requests.post('https://api.atlassian.com/ex/jira/{}/rest/api/3/issue/{}/transitions'.format(cloud_id, create_issue_res['id']),
-                                   headers={'Authorization': 'Bearer {}'.format(res.json()['access_token'])},
-                                   json={'transition':{'id':'31'}}).status_code)
-else:
-    print(res.status_code)
-    print(json.dumps(res.json()))
-
+(username, token) = cur.execute('select username, token from auth').fetchone()
+(url, project, issuetype, assignee, transition, space_id, parent) = cur.execute('select * from config').fetchone()
+create_issue_res = requests.post('https://{}/rest/api/3/issue'.format(url),
+                     auth=(username, token), json={
+        'fields': {'summary': ping_summary(),
+                   'project': {'key': project}, 'issuetype': {'id': issuetype}, 'assignee': {'id': assignee}}}).json()
+print(json.dumps(create_issue_res))
+print(requests.post('https://{}/rest/api/3/issue/{}/transitions'.format(url, create_issue_res['id']),
+                    auth=(username, token),
+                    json={'transition':{'id':transition}}).status_code)
+print(requests.post('https://{}/wiki/api/v2/pages'.format(url),
+                    auth=(username, token), json={
+        'spaceId': space_id,'status': 'current','title': ping_summary(), 'parentId': parent,
+        'body': {'representation': 'storage', 'value': ''}
+    }).json())
 con.close()
--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
-CREATE TABLE token (contents TEXT NOT NULL UNIQUE); 
+CREATE TABLE auth (username TEXT, token TEXT);
+CREATE TABLE config (url TEXT, project TEXT, issuetype TEXT, assignee TEXT, transition TEXT, space_id TEXT, parent TEXT);
