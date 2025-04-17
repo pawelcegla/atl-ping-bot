66305e4bb9716585935c5f29adfa2fb087dab109
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2025-04-17 12:42:57 +0200

    Remove notifications (#3)
    * Don't send notifications
    
    * Remove notifications config

--- schema.sql
+++ schema.sql
@@ -1,3 +1,2 @@
 CREATE TABLE auth (username TEXT, token TEXT);
 CREATE TABLE config (url TEXT, project TEXT, issuetype TEXT, assignee TEXT, transition TEXT, space_id TEXT, parent TEXT);
-CREATE TABLE webhook (url TEXT);
